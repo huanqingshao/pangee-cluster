7b6729a9c6ca4e52ef73a3482f1d10cb7b105051
huanqingshao <shaohuanqing@chinamobile.com>
2025-05-12 16:08:53 +0800

    优化 markdown 样式

--- web/src/views/clusters/plan/PlanArchitecture.vue
+++ web/src/views/clusters/plan/PlanArchitecture.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
-    <div v-if="markdownContent" v-html="docsHtml" class="architecture-markdown"></div>
-    <div id="architecture-markdown-image-modal" class="architecture-markdown-image-modal"
+    <div v-if="markdownContent" v-html="docsHtml" class="plan-markdown"></div>
+    <div id="architecture-markdown-image-modal" class="plan-markdown-image-modal"
       onclick="hideArchMarkdownImageModal()">
-      <img id="architecture-markdown-image-modal-image" class="architecture-markdown-image-modal-content">
+      <img id="architecture-markdown-image-modal-image" class="plan-markdown-image-modal-content">
     </div>
   </div>
 </template>
@@ -74,125 +74,6 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped>
-.architecture-markdown-image-modal {
-  z-index: 900000;
-  display: none;
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background: rgba(0, 0, 0, 0.8);
-  cursor: pointer;
-}
-
-.architecture-markdown-image-modal-content {
-  max-width: 90%;
-  max-height: 90%;
-  margin: auto;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  position: absolute;
-}
-</style>
-
 <style lang="scss">
-.architecture-markdown {
-  counter-reset: h1counter h2counter h3counter h4counter h5counter;
-}
-
-.architecture-markdown {
-  font-family: Consolas, Menlo, Bitstream Vera Sans Mono, Monaco, "微软雅黑", monospace;
-  font-size: 14px;
-
-
-  h1 {
-    font-size: 18px;
-    counter-reset: h2counter 1;
-  }
-
-  h1::before {
-    counter-increment: h1counter;
-    content: counter(h1counter) ". ";
-  }
-
-  h2 {
-    font-size: 16px;
-    counter-reset: h3counter;
-  }
-
-  h2::before {
-    counter-increment: h2counter;
-    content: counter(h1counter)"." counter(h2counter) " ";
-  }
-
-  h3 {
-    font-size: 15px;
-    counter-reset: h4counter;
-  }
-
-  h3::before {
-    counter-increment: h3counter;
-    content: counter(h1counter)"." counter(h2counter)"." counter(h3counter) " ";
-  }
-
-  h4 {
-    counter-reset: h5counter;
-  }
-
-  h4::before {
-    counter-increment: h4counter;
-    content: counter(h1counter)"." counter(h2counter)"." counter(h3counter) "." counter(h4counter) " ";
-  }
-
-  h5::before {
-    counter-increment: h5counter;
-    content: counter(h1counter)"." counter(h2counter)"." counter(h3counter) "." counter(h4counter) "." counter(h5counter) " ";
-  }
-
-  p img {
-    max-width: 900px;
-    cursor: zoom-in;
-  }
-
-  p code {
-    color: #c7254e !important;
-    padding: .25rem .5rem;
-    margin: 0;
-    font-size: .9em;
-    background-color: rgba(27, 31, 35, .05);
-    border-radius: 3px;
-    font-family: Consolas, Menlo, "Bitstream Vera Sans Mono", Monaco, "微软雅黑", monospace;
-  }
-
-  li code {
-    color: #c7254e !important;
-    padding: 1px .5rem;
-    margin: 0;
-    font-size: .9em;
-    border-radius: 3px;
-    background-color: rgba(27, 31, 35, .05);
-    font-family: Consolas, Menlo, "Bitstream Vera Sans Mono", Monaco, "微软雅黑", monospace;
-  }
-
-  pre {
-    padding-left: .5rem;
-    vertical-align: middle;
-    line-height: 1.4;
-    padding: 0.8rem 1rem;
-    margin: .85rem 0;
-    background-color: #606266;
-    color: #fff;
-    border-radius: 6px;
-    overflow: auto;
-
-    code {
-      font-family: Consolas, Menlo, "Bitstream Vera Sans Mono", Monaco, "微软雅黑", monospace;
-    }
-  }
-
-}
+@import "./markdown.scss";
 </style>
