bbd535dac0df1b75ab1c9c264de31956051f6eec
timothyeunofficial <timothyeunofficial@gmail.com>
2025-08-26 15:55:02 +0800

    时间格式修改

--- server/api/command/execute.go
+++ server/api/command/execute.go
@@ -88,7 +88,7 @@ func (execute *Execute) exec() {
 		}
 	}()
 
-	startTime := time.Now().Format(time.RFC3339Nano)
+	startTime := time.Now().Format("2006-01-02_15-04-05.999")
 	pid := startTime + "_" + execute.Type
 	if execute.Pid != "" {
 		pid = execute.Pid
